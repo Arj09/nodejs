be6e453d438d40933a5bc732089918cac7c1c3c6
Arj09 <arjunkushwah100199@gmail.com>
2023-07-10 21:35:57 +0530

    cors

--- Contact-backed/Server.js
+++ Contact-backed/Server.js
@@ -10,7 +10,12 @@ connectDb();
 const app = express();
 
 const port = process.env.PORT || 50001;
-app.use(cors())
+app.use(cors({
+    origin:["https://nodejs-mu.vercel.app/"],
+    methods:["POST","GET"],
+    credentials: true
+
+}))
 app.use(express.json());
 app.use('/api/contact', require('./routes/contactRoutes'));
 app.use(errorHandler);
